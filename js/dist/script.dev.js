b2f247b7ad5d6624b9fce65d990a632b041c9a8f
LucasFrostyWolf <lucas_baddogs@outlook.com>
2020-09-16 16:19:38 -0300

    :package: JS orçamento

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -3,3 +3,25 @@
 document.querySelector('.header__menu__hamburger').addEventListener('click', function () {
   return document.querySelector('.container__principal').classList.toggle('header__menu__show');
 });
+document.querySelector('#qtde').addEventListener("change", atualizarPreco);
+document.querySelector('#js').addEventListener("change", atualizarPreco);
+document.querySelector('#layout-sim').addEventListener("change", atualizarPreco);
+document.querySelector('#layout-nao').addEventListener("change", atualizarPreco);
+document.querySelector('#prazo').addEventListener("change", function () {
+  var prazo = document.querySelector('#prazo').value;
+  document.querySelector('label[for=prazo]').innerHTML = "Prazo: ".concat(prazo, " semanas");
+  atualizarPreco();
+});
+
+function atualizarPreco() {
+  var qtde = document.querySelector('#qtde').value;
+  var temJS = document.querySelector('#js').checked;
+  var layout = document.querySelector('#layout-sim').checked;
+  var prazo = document.querySelector('#prazo').value;
+  var preco = qtde * 100;
+  if (temJS) preco *= 1.1;
+  if (layout) preco += 500;
+  var taxaUrgencia = 1 - prazo * 0.1;
+  preco *= 1 + taxaUrgencia;
+  document.querySelector("#preco").innerHTML = "R$ ".concat(preco.toFixed(2));
+}
